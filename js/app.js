e4c4d284984478a2653e2599ca9497a934999727
Marilu Llamocca <maryleo.3007@gmail.com>
2017-05-26 16:39:24 -0500

    calc km

--- js/app.js
+++ js/app.js
@@ -15,7 +15,47 @@ function initMap() {
       navigator.geolocation.getCurrentPosition(funcionExito,funcionError);
     }
   }
+  var inputOrigen = document.getElementById("inputOrigen");
+  var inputDestino = document.getElementById("inputDestino");
+  new google.maps.places.Autocomplete(inputOrigen);
+  new google.maps.places.Autocomplete(inputDestino);
 
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
+  var calculateAndDisplayRoute = function (directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: inputOrigen.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING'
+    },function (response,status) {
+      if (status === 'OK') {
+
+        var distancia = Number((response.routes[0].legs[0].distance.text.replace(" km","")).replace(",","."));
+        console.log(response.routes[0].legs[0].distance.text);
+
+        tarifa.classList.remove("none");
+
+        var costo = distancia*1.75;
+        if(costo<4){
+          tarifa.innerHTML="S/. 4";
+        }
+        tarifa.innerHTML = "S./"+parseInt(costo);
+      }else{
+        window.alert("no encontramos la ruta");
+      }
+      directionsDisplay.setDirections(response);
+    });
+  }
+
+  directionsDisplay.setMap(map);
+
+  var trazaruta = function () {
+    calculateAndDisplayRoute(directionsService,directionsDisplay);
+  }
+
+  document.getElementById('trazaruta').addEventListener("click",trazaruta);
+  
   document.getElementById("encuentrame").addEventListener("click",buscar);
 
 	var latitud,longitud;
